0ae9d6ee640b9b1d3187867d64e4ac8370e6b264
DizzyRaisin <daneezhao@gmail.com>
2020-08-20 23:32:25 +1000

    first commit

---
+++ src/utils/OpenWeatherMap/OpenWeatherMap.js
@@ -0,0 +1,24 @@
+import axios from 'axios';
+require("dotenv").config();
+
+const baseURL = 'https://api.openweathermap.org/data/2.5';
+
+const OpenWeatherMap = axios.create({
+  baseURL,
+});
+
+const enrichRequestWithAppId = (config) => {
+  config.params.appid = process.env.REACT_APP_API_KEY;
+  return config;
+}
+
+const enrichRequestWithCelsiusUnits = (config) => {
+  config.params.units = 'metric';
+
+  return config;
+};
+
+OpenWeatherMap.interceptors.request.use(enrichRequestWithAppId);
+OpenWeatherMap.interceptors.request.use(enrichRequestWithCelsiusUnits);
+
+export default OpenWeatherMap;
